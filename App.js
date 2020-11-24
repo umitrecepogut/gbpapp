0b35256b9ed70ea6b6895a1da479774501ca348b
Ümit Recep ÖĞÜT <33132905+umitrecepogut@users.noreply.github.com>
2020-11-24 22:05:35 +0300

    Update App.js

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text style={{ fontSize: 30 }}>Good Bet Predictions</Text>
+      <Text style={{ fontSize: 30 }}>by Ümit Recep ÖĞÜT</Text>
       <StatusBar style='auto' />
     </View>
   );
