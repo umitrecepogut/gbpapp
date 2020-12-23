f828f3c21705bcafa67715a361eb7600d7d3a0c0
Ümit Recep ÖĞÜT <33132905+umitrecepogut@users.noreply.github.com>
2020-12-23 23:06:19 +0300

    yorum eklendi.

--- src/screens/CouponDetail/CouponDetailScreen.js
+++ src/screens/CouponDetail/CouponDetailScreen.js
@@ -8,6 +8,13 @@ const CouponDetailScreen = ({ navigation, coupon }) => {
 
   //    getParam fonksiyonu yok
   //    coupon u burda yakalayamadım
+  //    https://bankomaclarapi.herokuapp.com/coupons/
+  //    Yukarıdaki linke PUT ile
+  //    {
+  //      "couponId" : "5fe3a2485f82ba00245fbc63",
+  //      "userId" : "5fde0345843d7300247ac427"
+  //    }
+  //    gönderirsen Coupon un favUsers'ına ekleme yapacak. Eğer favUsersta da kullanıcı varsa sarı göstercez o kullanıcıya
 
   //    const coupon = navigation.getParam('coupon');
   //    console.log(coupon);
